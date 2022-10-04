f2cabc3bc03e87e9309e276419311ae46e1a09df
fatihmaras <fatihmmaras@gmail.com>
2022-10-04 03:23:37 +0300

    Class Example

---
+++ EB-T3_Class/Araba.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EB_T3_Class
+{
+    public class Araba
+    {
+        public int KapiSayisi;
+        public string ArabaModel;
+        public String ArabaRengi;
+
+
+        public Araba(int XkapiSayisi, String XarabaModel, String XarabaRengi)
+        {
+            KapiSayisi = XkapiSayisi;
+            ArabaModel = XarabaModel;
+            ArabaRengi = XarabaRengi;
+
+        }
+
+
+        public void MotorCalistir()
+        {
+            Console.WriteLine("Motor Çalışıyor...");
+        }
+
+        public void KapilariKilitle()
+        {
+            Console.WriteLine("Kapılar Kilitleniyor...");
+        }
+    }
+}
---
+++ EB-T3_Class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassKullanimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int KapiSayisi = 4;
+            //string ArabaModel = "Renault";
+            //string ArabaRengi = "Kırmızı";
+
+
+
+            Araba Araba1 = new Araba(4, "Renault", "Kırmızı");
+
+            Console.WriteLine("Araba Rengi : " + Araba1.ArabaRengi);
+            Console.WriteLine("Araba Modeli : " + Araba1.ArabaModel);
+            Console.WriteLine("Kapı Sayısı : " + Araba1.KapiSayisi);
+
+            //Araba1.KapiSayisi = 4;
+            //Araba1.ArabaRengi = "Kırımızı";
+            //Araba1.ArabaModel = "Renault";
+
+            //Araba1.KapilariKilitle();
+            //Araba1.MotorCalistir();
+
+            //Console.WriteLine("Arabanın Kapı Sayısı : " + Araba1.KapiSayisi);
+            //Console.WriteLine("Arabanın Rengi : " + Araba1.ArabaRengi);
+            //Console.WriteLine("Arabanın Modeli : " + Araba1.ArabaModel);
+
+            Console.ReadLine();
+
+        }
+    }
+}
